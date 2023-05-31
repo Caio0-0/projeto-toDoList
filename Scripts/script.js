3f5adcdb11435e3cc78d5292829520ce90891999
Caio0-0 <126476942+Caio0-0@users.noreply.github.com>
2023-05-31 14:17:26 -0300

    novo

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,18 +1,54 @@
-let input = document.getElementById("input-txt")
+let contador = 0
+let input = document.querySelector(".input-txt")
 let btnAdd = document.getElementById("btnAdd")
-let main = document.getElementById("areaLista")
+let main = document.querySelector(".areaLista")
 
 function addTarefa(){
     let valorInput = input.value
 
     if ((valorInput!=="") && (valorInput!==null) && (valorInput!==undefined)){
-        let novoItem = `<div class="item">
-        <div class="item-icone"><i class="fa-regular fa-circle"></i></div>
-        <div class="item-nome">Texto de teste</div>
-        <div class="item-botao"><button class="delete"><i class="fa-solid fa-trash"></i></button></div>
+
+        ++contador;
+
+        let novoItem = `<div id="${contador}" class="item">
+        <div onclick="marcarTarefa(${contador})" class="item-icone"><i id="icone_${contador}" class="cicle"></i></i></div>
+        <div class="item-nome" onclick="marcarTarefa(${contador})">${valorInput}</div>
+        <div class="item-botao"><button onclick="deletar(${contador})" class="delete"><i>X</i></button></div>
         
     </div>`
         main.innerHTML +=novoItem
+        input.value =""
+        input.focus();
 
     }
 }
+function deletar(id){
+    var tarefa = document.getElementById(id);
+    tarefa.remove();
+}
+function marcarTarefa(id){
+    var item = document.getElementById(id)
+    var classe = item.getAttribute("class")
+
+    if (classe=="item"){
+        item.classList.add("item-clicada")
+
+        var icone = document.getElementById("icone_"+id)
+        icone.classList.remove("cicle")
+       icone.classList.add("cicle-marcado")
+
+        item.parentNode.appendChild(item)
+    }else{
+        item.classList.remove("item-clicada")
+
+        var icone = document.getElementById("icone_"+id)
+        icone.classList.remove("cicle-marcado")
+        icone.classList.add("cicle")
+    }
+}
+input.addEventListener("keyup",function(event){
+    if (event.keyCode ===13){
+        event.preventDefault()
+        btnAdd.click()
+    }
+})
